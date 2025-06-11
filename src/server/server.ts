01f381b3e6bbd58fb79eba072d5c46ab63782599
Kamatchi <p.kamatchi.netlink@gmail.com>
2025-06-11 17:12:15 +0400

    server and client setup

---
+++ src/server/server.ts
@@ -0,0 +1,20 @@
+import { createServer } from "http";
+import express, {Express } from "express";
+import { testHandler } from "./testHandler";
+import httpProxy from "http-proxy";
+import helmet from "helmet";
+const port = 5200;
+const expressApp: Express = express();
+const proxy = httpProxy.createProxyServer({
+    target: "http://localhost:5300", ws: true
+});
+expressApp.use(helmet());
+expressApp.use(express.json());
+expressApp.post("/test", testHandler);
+expressApp.use(express.static("static"));
+expressApp.use(express.static("node_modules/bootstrap/dist"));
+expressApp.use((req, resp) => proxy.web(req, resp));
+const server = createServer(expressApp);
+server.on('upgrade', (req, socket, head) => proxy.ws(req, socket, head));
+server.listen(port,
+    () => console.log(`HTTP Server listening on port ${port}`));
